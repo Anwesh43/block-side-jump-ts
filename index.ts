9abbecef039580d85a83bed6843dc4c2d338e2c0
Anwesh43 <anweshthecool0@gmail.com>
2021-03-15 01:07:12 +0530

    adding BlockSideJump class

--- index.ts
+++ index.ts
@@ -173,3 +173,26 @@ class BSJNode {
         return this 
     }
 }
+
+class BlockSideJump {
+
+    curr : BSJNode = new BSJNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
