9e97657cc44e8d3ee12b6f9d5c3e6b463e79481a
Anwesh43 <anweshthecool0@gmail.com>
2021-03-15 01:04:01 +0530

    adding BSJNode class

--- index.ts
+++ index.ts
@@ -131,3 +131,45 @@ class Animator {
         }
     }
 }
+
+class BSJNode {
+
+    prev : BSJNode 
+    next : BSJNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BSJNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBSJNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : BSJNode {
+        var curr : BSJNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
